07fdd9a1200b1536045751d3994026e4de54771e
Shuto Takahahsi <m59rt@yahoo.co.jp>
2021-07-25 12:34:23 +0900

    [Delete] API key

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import random
 import wandb
 
 
-os.environ["WANDB_API_KEY"] = "44207cf34c89cbe5361e5ec07f0044cc8e5e583d"
-
-
 def generate_random_number(step: int) -> float:
     return math.log(0.1 + random.random() + step * 0.01)
 
