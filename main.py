a62671f7fef4e1f28f53ba897d31c2a632c9d880
Shuto Takahahsi <m59rt@yahoo.co.jp>
2021-07-25 12:12:13 +0900

    [Add] Saving code example

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import math
+import random
+import wandb
+
+
+os.environ["WANDB_API_KEY"] = "44207cf34c89cbe5361e5ec07f0044cc8e5e583d"
+
+
+def generate_random_number(step: int) -> float:
+    return math.log(0.1 + random.random() + step * 0.01)
+
+
+config = dict(hyperparameter=4,)
+run = wandb.init(project="my-test-project", config=config, save_code=False)
+
+with run:
+    for step in range(100):
+        wandb.log(
+            {
+                "acc": generate_random_number(step),
+                "val_acc": generate_random_number(step),
+                "loss": wandb.config.hyperparameter - generate_random_number(step),
+                "val_loss": wandb.config.hyperparameter - generate_random_number(step),
+            }
+        )
